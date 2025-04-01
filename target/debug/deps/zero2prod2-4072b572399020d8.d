202fa362657a1de9bde835a5b68eed7a22b6f77b
petarjuki7 <petar.jukic7@gmail.com>
2025-04-01 20:57:55 +0200

    Project skeleton

---
+++ target/debug/deps/zero2prod2-4072b572399020d8.d
@@ -0,0 +1,5 @@
+/Users/petarjukic/Documents/zero2prod2/zero2prod2/target/debug/deps/libzero2prod2-4072b572399020d8.rmeta: src/main.rs
+
+/Users/petarjukic/Documents/zero2prod2/zero2prod2/target/debug/deps/zero2prod2-4072b572399020d8.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/zero2prod2-acb5f73550af90f1.d
@@ -0,0 +1,5 @@
+/Users/petarjukic/Documents/zero2prod2/zero2prod2/target/debug/deps/libzero2prod2-acb5f73550af90f1.rmeta: src/main.rs
+
+/Users/petarjukic/Documents/zero2prod2/zero2prod2/target/debug/deps/zero2prod2-acb5f73550af90f1.d: src/main.rs
+
+src/main.rs:
