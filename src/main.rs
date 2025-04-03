ff18920c928dff003ea7743269d8570ec725febf
petarjuki7 <petar.jukic7@gmail.com>
2025-04-03 18:04:39 +0200

    updated tests and CI

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::{io::Error, net::TcpListener};
 
+use zero2prod2::configuration::get_configuration;
 use zero2prod2::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let listener = TcpListener::bind("127.0.0.1:0")?;
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
     println!(
         "Listening on port: {}",
         TcpListener::local_addr(&listener).unwrap().port()
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,7 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
+use zero2prod2::configuration::get_configuration;
 use zero2prod2::startup::run;
 
 fn spawn_app() -> String {
@@ -28,6 +30,11 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let addr = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -40,6 +47,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
